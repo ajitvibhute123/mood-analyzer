3d1e4fa89e5d2df25feec40ea988655a5e6caa0e
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-04-03 22:44:32 +0530

    Added :  inform used if entered invalid mood

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertSame;
 
@@ -7,26 +8,13 @@ import static org.junit.Assert.assertSame;
 public class MoodAnalyserTest {
 
     @Test
-    public void given_SadMood_Should_Return_SAD() {
-        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain("I am in Sad Mood");
+    public void given_NullMood_Should_Throw_MoodAnalysisException() {
+        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain(null);
         String mood;
         try {
             mood = moodAnalyser.analyseMood();
-            assertSame("SAD", mood);
         } catch (MoodAnalysisException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test
-    public void given_AnyMood_Should_Return_HAPPY() {
-        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain("I am in Happy Mood");
-        String mood;
-        try {
-            mood = moodAnalyser.analyseMood();
-            assertSame("HAPPY", mood);
-        } catch (MoodAnalysisException e) {
-            e.printStackTrace();
+            Assertions.assertSame(MoodAnalysisException.Exception_Type.NULL, e.type);
         }
     }
 }
