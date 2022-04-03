fb8d54f29faa66a3acfac3ddd9150130406f48df
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-04-03 16:53:35 +0530

    Initial commit

---
+++ src/main/java/MoodAnalyserMain.java
@@ -0,0 +1,16 @@
+
+/**
+ * Purpose -Welcome message
+ * 
+ * @author Ajit Vibhute
+ * @since - 02-04-2022
+ *
+ */
+
+public class MoodAnalyserMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to mood analyzer program");
+	}
+
+}
