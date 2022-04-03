845240fd9b20c61bd7d82094ee8ab391f5790e3c
ajitvibhute123 <vibhuteajit3@gmail.com>
2022-04-03 22:50:09 +0530

    Added : empty mood should throw mood analysis exception.

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -7,6 +7,7 @@ import static org.junit.Assert.assertSame;
  */
 public class MoodAnalyserTest {
 
+
     @Test
     public void given_NullMood_Should_Throw_MoodAnalysisException() {
         MoodAnalyserMain moodAnalyser = new MoodAnalyserMain(null);
@@ -15,6 +16,16 @@ public class MoodAnalyserTest {
             mood = moodAnalyser.analyseMood();
         } catch (MoodAnalysisException e) {
             Assertions.assertSame(MoodAnalysisException.Exception_Type.NULL, e.type);
+        }
+    }
+    @Test
+    public void given_EmptyMood_Should_Throw_MoodAnalysisException() {
+        MoodAnalyserMain moodAnalyser = new MoodAnalyserMain("");
+        String mood;
+        try {
+            mood = moodAnalyser.analyseMood();
+        } catch (MoodAnalysisException e) {
+            Assertions.assertSame(MoodAnalysisException.Exception_Type.EMPTY, e.type);
         }
     }
 }
